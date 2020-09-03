93db147f90f90a9447f60f525ae882a9040037da
bvc5434 <bvc5434@psu.edu>
2020-09-03 15:17:06 +0000

    -changed the if statements

--- main.py
+++ main.py
@@ -4,8 +4,9 @@
 temp = input("Enter temperature: ")
 temp = float(temp)
 units = input("Enter unit in F/f or C/c: ")
-if (units == "C" or "c"):
+if (units == "C" or units == "c"):
   print(f"{temp}° in Celsius is equivalent to {32+((temp/5)*9)}° Fahrenheit.")
-elif (units == "F" or "f"):
+elif (units == "F" or units == "f"):
   print(f"{temp}° in Fahrenheit is equivalent to {5*(temp-32)//9}° Celsius.")
-else:print("Invalid unit({units}).")
+else:
+  print(f"Invalid unit({units}).")
